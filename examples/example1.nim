237cba6df2bcd1fe17ac3d98b93f7ff674089e76
Remigijus Jodelis <remigijus@toptal.com>
2018-11-14 18:15:56 +0200

    show combined scatter-contour plot

--- examples/example1.nim
+++ examples/example1.nim
@@ -12,7 +12,7 @@ let (grid, scale) = planarGrid(x.limit, grid_step)
 let pred = model.predict(grid)
 
 if show:
-  showScatter(x, y, "Planar scatter")
-  showContour(pred.asGrid, scale, "Decision boundary")
-  # showCombined(x, y, pred.asGrid, scale, "Planar model")
+  # showScatter(x, y, "Planar scatter")
+  # showContour(pred.asGrid, scale, "Decision boundary")
+  showCombined(x, y, pred.asGrid, scale, "Planar model")
   # showLines(scores, "Accuracy score")
--- src/plot.nim
+++ src/plot.nim
@@ -3,7 +3,8 @@ import arraymancer, plotly, chroma, sequtils
 const
   red  = Color(r: 0.9, g: 0.0, b: 0.0, a: 1.0)
   blue = Color(r: 0.0, g: 0.0, b: 0.9, a: 1.0)
-
+  white = Color(r: 0.9, g: 0.9, b: 0.9, a: 1.0)
+  black = Color(r: 0.0, g: 0.0, b: 0.0, a: 1.0)
 
 proc showScatter*(xs, ys: Tensor[float], title="Plot", plotsize=500, dotsize=8.0): void =
   let d = Trace[float](mode: PlotMode.Markers, `type`: PlotType.Scatter)
@@ -87,4 +88,38 @@ proc showLines*(data: seq[float], title="Plot"): void =
       autosize: false)
 
   let p = Plot[float](layout: layout, traces: @[d])
+  p.show()
+
+
+proc showCombined*(xs, ys: Tensor[float], zs: seq[seq[float]], scale: seq[float],
+                   title="Plot", plotsize=600, dotsize=4.0): void =
+  let d1 = Trace[float](mode: PlotMode.Lines, `type`: PlotType.Contour)
+  let d2 = Trace[float](mode: PlotMode.Markers, `type`: PlotType.Scatter)
+
+  d1.zs = zs
+  d1.xs = scale
+  d1.ys = scale
+  d1.colorscale = ColorMap.Bluered
+  d1.heatmap = true
+  d1.contours = (0.0, 1.0, 0.5)
+
+  let m = ys.shape[0]
+  let pts = xs.astype(float)
+  d2.xs = pts[_, 0].reshape(m).data()
+  d2.ys = pts[_, 1].reshape(m).data()
+
+  let dotsizes = @[dotsize]
+  let red_blue = [black, white]
+  let colors = ys.astype(float).data().mapIt(red_blue[it.int])
+  d2.marker = Marker[float](size: dotsizes, color: colors)
+
+  let layout = Layout(
+    title: title,
+    width: plotsize,
+    height: plotsize,
+    xaxis: Axis(title: "x-axis"),
+    yaxis: Axis(title: "y-axis"),
+    autosize: true)
+
+  let p = Plot[float](layout: layout, traces: @[d1, d2])
   p.show()
